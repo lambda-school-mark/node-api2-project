7f03911c65ed1f11243c7ac6c74258bc5cdeadeb
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-04 17:49:42 -0400

    update post by id

--- routers/posts/posts-router.js
+++ routers/posts/posts-router.js
@@ -98,4 +98,50 @@ router.get("/:id/comments", (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    Posts.remove(id)
+      .then((post) => {
+        res.status(200).json({ message: `deleted post ${id}` });
+      })
+      .catch((error) => {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      });
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    if (id) {
+      if (req.body.title && req.body.contents) {
+        Posts.update(id, req.body)
+          .then((updatedPost) => {
+            res.status(200).json(updatedPost);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        res.status(400).json({
+          errorMessage: "Please provide title and contents for the post.",
+        });
+      }
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
+  }
+});
+
 module.exports = router;
